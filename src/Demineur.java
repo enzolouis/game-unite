802a12b3257d8621a29209f6acbe7ae1b9bdab83
EmberOn <ember.contactme@gmail.com>
2023-09-15 15:45:03 +0200

    test branch2

--- src/Demineur.java
+++ src/Demineur.java
@@ -6,6 +6,7 @@ public class Demineur {
 	private int length;
 	private int[][] grid;
 	private ArrayList<Point> bombes;
+	private int test2;
 
 	public Demineur(int length) {
 		this.length = length;
