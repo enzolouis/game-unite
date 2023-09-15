71fe2263e4fa6ccd85f307b5359cb70d03c6edef
EmberOn <ember.contactme@gmail.com>
2023-09-15 22:33:00 +0200

    implements click on empty case (recursive)

--- src/Demineur.java
+++ src/Demineur.java
@@ -1,6 +1,12 @@
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Demineur {
 	private int length;
@@ -66,9 +72,56 @@ public class Demineur {
 	public void showGrid() {
 		for (int i = 0; i < this.length; i++) {
 			for (int j = 0; j < this.length; j++) {
-				System.out.print(this.grid[i][j] + " ");
+				System.out.print(((this.grid[i][j] == 0) ? " " : this.grid[i][j]) + " ");
 			}
 			System.out.println(" ");
+		}
+	}
+	
+	public void showCaseWithNumberNearEmptyCase(List<Point> points) {
+		points.add(new Point(-1, -1));
+		List<Point> points_temp = new ArrayList<>(points);
+		for (Point p : points_temp) {
+			for (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					if (i == 0 && j == 0) {
+						continue;
+					}
+					
+					int pos1 = p.x + i;
+					int pos2 = p.y + j;
+					
+					if (!points.contains(new Point(pos1, pos2))) {
+						if (pos1 >= 0 && pos1 < this.length && pos2 >= 0 && pos2 < this.length) {
+							if (this.grid[pos1][pos2] != 0 && this.grid[pos1][pos2] != 9) {
+								points.add(new Point(pos1, pos2));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void recursiveDeployEmptyCase(Point p, List<Point> points) {
+		if (grid[p.x][p.y] == 0) {
+			for (int h = -1; h <= 1; h++) {
+				for (int k = -1; k <= 1; k++) {
+					if (h == 0 && k == 0) {
+						continue; // On ne doit pas compter la case sur laquel on est
+					}
+					int pos1 = p.x + h;
+					int pos2 = p.y + k;
+					if (!points.contains(new Point(pos1, pos2))) {
+						if (pos1 >= 0 && pos1 < this.length && pos2 >= 0 && pos2 < this.length) {
+							if (this.grid[pos1][pos2] == 0) {
+								points.add(new Point(pos1, pos2));
+								recursiveDeployEmptyCase(new Point(pos1, pos2), points);
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
@@ -77,5 +130,18 @@ public class Demineur {
 		demineur.generateGrid();
 		demineur.showGrid();
 
+		List<Point> p = new ArrayList<Point>();
+		
+		demineur.recursiveDeployEmptyCase(new Point(5, 5), p);
+		demineur.showCaseWithNumberNearEmptyCase(p);
+		System.out.println("-------");
+		System.out.println("-------");
+		for (Point po : p) {
+			System.out.println("(" + po.x + "," + po.y + ")");
+			
+			if (po.x != -1) // et y != -1 (test tempo)
+				demineur.grid[po.x][po.y] = 8;
+		}
+		demineur.showGrid();
 	}
 }
